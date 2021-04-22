b8bb89b384a02615e0ac79e8f241ac633f26bb6f
igorxciv <icheliadinski@gmail.com>
2021-04-22 10:23:59 -0400

    new: add radio option styles

--- libs/ui-kit/src/lib/atoms/radio/Radio.tsx
+++ libs/ui-kit/src/lib/atoms/radio/Radio.tsx
@@ -1,20 +1,34 @@
 import React, { FC, PropsWithChildren } from 'react'
+import classNames from 'classnames'
+import styles from './Radio.module.scss'
 
 type Props = {
   disabled?: boolean
+  appearance?: 'basic' | 'primary' | 'info' | 'success' | 'warning' | 'danger'
+  checked?: boolean
 }
 
 export const Radio: FC<Props> = (props: PropsWithChildren<Props>) => {
-  const { disabled, children } = props
+  const { disabled, checked, children } = props
+
+  const radioClasses = classNames(styles['radio'])
+  const inputClasses = classNames(styles['radio__input'])
+  const circleClasses = classNames(styles['radio__circle'])
+  const labelClasses = classNames(styles['radio__label'])
 
   return (
-    <label>
-      <input type="radio" disabled={disabled} />
-      {children}
+    <label className={radioClasses}>
+      <input type="radio" disabled={disabled} checked={checked} className={inputClasses} />
+      <span className={circleClasses} />
+      <span className={labelClasses}>
+        {children}
+      </span>
     </label>
   )
 }
 
 Radio.defaultProps = {
   disabled: false,
+  appearance: 'basic',
+  checked: false,
 }
