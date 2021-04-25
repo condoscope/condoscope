d621f71d91881b78ad1f148b11d269cf7f6b130e
igorxciv <icheliadinski@gmail.com>
2021-04-25 10:17:48 -0400

    new: add chevron animation

--- libs/ui-kit/src/lib/atoms/select/Select.tsx
+++ libs/ui-kit/src/lib/atoms/select/Select.tsx
@@ -10,11 +10,12 @@ type Props = {
 export const Select: FC<Props> = (props: PropsWithChildren<Props>) => {
   const { appearance } = props
 
+  const groupClasses = classNames(styles['select__group'])
   const selectedClasses = classNames(styles['select__selected'], styles[appearance])
   const chevronClasses = classNames(styles['select__chevron'])
 
   return (
-    <details>
+    <details className={groupClasses}>
       <summary className={selectedClasses}>
         Option 1
         <ChevronIcon className={chevronClasses} />
