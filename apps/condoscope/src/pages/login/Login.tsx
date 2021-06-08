ccb6c0a1aa1f74f9d2114323328f83072bf79412
igorxciv <icheliadinski@gmail.com>
2021-06-07 22:07:16 -0400

    new: add section styles

--- apps/condoscope/src/pages/login/Login.tsx
+++ apps/condoscope/src/pages/login/Login.tsx
@@ -1,25 +1,33 @@
 import { FC } from 'react'
 import { Input } from '@condoscope/ui-kit'
+import classNames from 'classnames'
+import styles from './Login.module.scss'
 
 export const Login: FC = () => {
+  const pageClasses = classNames(styles['login'])
+  const sectionClasses = classNames(styles['login__section'])
+  const titleClasses = classNames(styles['login__title'])
+
   return (
-    <section>
-      <h1>
-        Login
-      </h1>
-      <span>
-        Hello! Log in with your e-mail
-      </span>
+    <main className={pageClasses}>
+      <section className={sectionClasses}>
+        <h1 className={titleClasses}>
+          Login
+        </h1>
+        <span>
+          Hello! Log in with your e-mail
+        </span>
 
-      <form>
-        <Input placeholder="E-mail address" type="email">
-          Email address
-        </Input>
+        <form>
+          <Input placeholder="E-mail address" type="email">
+            Email address
+          </Input>
 
-        <Input placeholder="Password" type="password">
-          Password
-        </Input>
-      </form>
-    </section>
+          <Input placeholder="Password" type="password">
+            Password
+          </Input>
+        </form>
+      </section>
+    </main>
   )
 }
